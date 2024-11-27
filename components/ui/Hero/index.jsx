a8052f566fa0b712438db6bc5f87543ab7f99d31
biodun-dev <abdultoheeb97@gmail.com>
2024-11-27 11:56:10 +0100

    fix

--- components/ui/Hero/index.jsx
+++ components/ui/Hero/index.jsx
@@ -42,15 +42,17 @@ const Hero = () => {
                 : "opacity-0 blur-sm translate-y-10"
             }`}
           >
+            {/* Primary Button */}
             <NavLink
               href="/services"
-              className="text-blue-800 bg-white border border-white hover:bg-blue-600 hover:text-white px-5 py-3 rounded-lg transition duration-150"
+              className="bg-blue-600 text-white hover:bg-blue-700 hover:shadow-lg px-5 py-3 rounded-lg transition-all duration-200"
             >
               Explore Services
             </NavLink>
+            {/* Secondary Button with Improved Hover */}
             <NavLink
               href="/case-studies"
-              className="text-red-600 bg-white border border-red-600 hover:bg-red-600 hover:text-white px-5 py-3 rounded-lg transition duration-150"
+              className="bg-gradient-to-br from-red-100 to-red-200 text-red-600 hover:bg-gradient-to-br hover:from-pink-500 hover:to-red-500 hover:text-white px-5 py-3 rounded-lg transition-all duration-200 shadow-lg"
             >
               View Case Studies
             </NavLink>
