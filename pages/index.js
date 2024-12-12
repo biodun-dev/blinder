6832a77262548c2cd5257f3ad6529b3612b5abd8
biodun-dev <abdultoheeb97@gmail.com>
2024-12-12 07:55:42 +0100

    break

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       <GradientWrapper>
         <Testimonials />
       </GradientWrapper> */}
-      <FooterCTA />
+      {/* <FooterCTA /> */}
     </>
   );
 }
