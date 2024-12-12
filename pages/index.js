c8f4658b9b62a77c57ceead5d22eddd693e5ea49
biodun-dev <abdultoheeb97@gmail.com>
2024-12-12 07:46:06 +0100

    revert

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,37 @@
-// import Head from "next/head";
-// import GradientWrapper from "../components/GradientWrapper";
-// import CTA from "../components/ui/CTA";
-// import Features from "../components/ui/Features";
-// import FooterCTA from "../components/ui/FooterCTA";
-// import Hero from "../components/ui/Hero";
-// import LogoGrid from "../components/ui/LogoGrid";
-// import Testimonials from "../components/ui/Testimonials";
-// import InsightsAndTrends from "../components/ui/Insights";
-// import YouTubeEmbed from "../components/ui/YouTubeEmbed";
-// import ClientSolutions from "../components/ui/ClientSolutions"
+import Head from "next/head";
+import GradientWrapper from "../components/GradientWrapper";
+import CTA from "../components/ui/CTA";
+import Features from "../components/ui/Features";
+import FooterCTA from "../components/ui/FooterCTA";
+import Hero from "../components/ui/Hero";
+import LogoGrid from "../components/ui/LogoGrid";
+import Testimonials from "../components/ui/Testimonials";
+import InsightsAndTrends from "../components/ui/Insights";
+import YouTubeEmbed from "../components/ui/YouTubeEmbed";
+import ClientSolutions from "../components/ui/ClientSolutions"
 
 
-// export default function Home() {
-//   return (
-//     <>
-//       <Head>
-//         <meta name='robots' content='index' />
-//       </Head>
-//       <Hero />
-//       {/* <LogoGrid /> */}
-//       <Features />
-//       {/* <YouTubeEmbed/> */}
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name='robots' content='index' />
+      </Head>
+      <Hero />
+      {/* <LogoGrid /> */}
+      <Features />
+      {/* <YouTubeEmbed/> */}
     
-//       <GradientWrapper>
+      <GradientWrapper>
      
-//         <CTA />
-//       </GradientWrapper>
-//       < ClientSolutions/>
-//       <InsightsAndTrends />
-//       <GradientWrapper>
-//         <Testimonials />
-//       </GradientWrapper>
-//       <FooterCTA />
-//     </>
-//   );
-// }
+        <CTA />
+      </GradientWrapper>
+      < ClientSolutions/>
+      <InsightsAndTrends />
+      <GradientWrapper>
+        <Testimonials />
+      </GradientWrapper>
+      <FooterCTA />
+    </>
+  );
+}
